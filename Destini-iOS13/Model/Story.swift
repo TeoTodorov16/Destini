b60bf3c8e40feb82a916a98a8a9749ce95c70326
Teodor <teodorttodorov@icloud.com>
2024-01-10 18:21:12 +0200

    + 1

--- Destini-iOS13/Model/Story.swift
+++ Destini-iOS13/Model/Story.swift
@@ -11,12 +11,16 @@ import Foundation
 struct Story {
     
     let title: String
-    let choice1: String
-    let choice2: String
-    
-    init(title: String, choice1: String, choice2: String) {
-        self.title = title
-        self.choice1 = choice1
-        self.choice2 = choice2
-    }
+       let choice1: String
+       let choice1Destination: Int  // Represents the index of the next story for choice 1
+       let choice2: String
+       let choice2Destination: Int  // Represents the index of the next story for choice 2
+       
+       init(title: String, choice1: String, choice1Destination: Int, choice2: String, choice2Destination: Int) {
+           self.title = title
+           self.choice1 = choice1
+           self.choice1Destination = choice1Destination
+           self.choice2 = choice2
+           self.choice2Destination = choice2Destination
+       }
 }
